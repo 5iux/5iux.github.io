a8b129763e0c370388f3effc50d4ebcfa09f483a
5iux <13775651+5iux@users.noreply.github.com>
2020-02-18 19:02:02 +0800

    Update sou.js

--- sou.js
+++ sou.js
@@ -43,10 +43,12 @@ $(document).ready(function() {
     $("#menu").click(function(event) {
         $(this).toggleClass('on');
         $(".list").toggleClass('closed');
+        $(".mywth").toggleClass('hidden');
     });
     $("#content").click(function(event) {
         $(".on").removeClass('on');
         $(".list").addClass('closed');
+        $(".mywth").removeClass('hidden');
     });
     $(".mywth").click(function(event) {
         var wt = $("body").width();
